9e3cea7f757db4c6ea66ab0ba7db4e42b7ece97e
Jonny Pillar <info@jonnypillar.co.uk>
2021-11-06 12:43:36 +0000

    Added EventProcessor Lambda
    - Takes events from an SQS queue and sends them to the EvilCorp API
    - Logs the success or failure for use in the Breaker

---
+++ EventProcessor/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/sirupsen/logrus"
+)
+
+func main() {
+	lambda.Start(Handle)
+}
+
+func Handle(ctx context.Context, sqsEvent events.SQSEvent) error {
+	log := logrus.New()
+
+	resp, err := http.Get("http://localhost:8090/api/poll")
+	if err != nil {
+		log.Error("error making request", err)
+
+		return err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		log.WithField("api-error", "error-occurred").
+			Error("api returned error: ", resp.StatusCode)
+
+		return fmt.Errorf("api error, %d", resp.StatusCode)
+	}
+
+	log.Infof("Successful API request %d", resp.StatusCode)
+
+	// Do something with the returned request
+
+	return nil
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,14 @@
 module github.com/JonnyPillar/CircuitBreakerDemo
 
 go 1.17
+
+require (
+	github.com/aws/aws-lambda-go v1.27.0 // indirect
+	github.com/aws/aws-sdk-go-v2 v1.11.0 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.1.0 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.0.0 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sqs v1.11.0 // indirect
+	github.com/aws/smithy-go v1.9.0 // indirect
+	github.com/sirupsen/logrus v1.8.1 // indirect
+	golang.org/x/sys v0.0.0-20191026070338-33540a1f6037 // indirect
+)
